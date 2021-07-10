23bcbf90339183a2dec2bd9be471c1972d8d22e9
dongminyoon <ehdals8965@naver.com>
2021-07-10 17:46:23 +0900

    setup: 폴더링

---
+++ ONETHING_iOS/ONETHING_iOS/Source/Model/TestModel.swift
@@ -0,0 +1,8 @@
+//
+//  Testmodel.swift
+//  ONETHING_iOS
+//
+//  Created by Dongmin on 2021/07/10.
+//
+
+import Foundation
---
+++ ONETHING_iOS/ONETHING_iOS/Source/Network/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  ONETHING_iOS
+//
+//  Created by Dongmin on 2021/07/10.
+//
+
+import Foundation
