2b1e38e04f05b7a11f1b70aef34293dc33bb9268
dongminyoon <ehdals8965@naver.com>
2021-07-10 17:32:59 +0900

    setup: 초기 프로젝트 생성

---
+++ ONETHING_iOS/ONETHING_iOS/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ONETHING_iOS
+//
+//  Created by Dongmin on 2021/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
