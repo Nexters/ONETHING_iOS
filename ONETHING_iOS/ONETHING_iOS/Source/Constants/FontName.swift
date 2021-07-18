eb252b0386384ea04451ffc29e9dd95a0e68ecb7
dongminyoon <ehdals8965@naver.com>
2021-07-18 20:29:38 +0900

    [fix] PR 리뷰 반영

--- ONETHING_iOS/ONETHING_iOS/Source/Constants/FontName.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/Constants/FontName.swift
@@ -9,21 +9,21 @@ import Foundation
 
 struct FontName {
     
-    static let prentendard = "Pretendard"
-    static let montserrat = "Montserrat"
-    
     static var pretendard_semibold: String { return self.prentendard + "-" + Weight.semiBold.rawValue }
     static var pretendard_bold: String { return self.prentendard + "-" + Weight.bold.rawValue }
     static var pretendard_extrabold: String { return self.prentendard + "-" + Weight.extraBold.rawValue }
     static var pretendard_light: String { return self.prentendard + "-" + Weight.light.rawValue }
     static var pretendard_medium: String { return self.prentendard + "-" + Weight.medium.rawValue }
     static var pretendard_regular: String { return self.prentendard + "-" + Weight.regular.rawValue }
+    
+    private static let prentendard = "Pretendard"
+    private static let montserrat = "Montserrat"
 
 }
 
 extension FontName {
     
-    enum Weight: String {
+    private enum Weight: String {
         case bold = "Bold"
         case extraBold = "ExtraBold"
         case extraLight = "ExtraLight"
