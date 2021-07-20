e376686d3959c303cd9340f7c5e140d1910fe947
ehgud0670 <kimdo2297@gmail.com>
2021-07-20 13:27:21 +0900

    [fix] 셀 터치를 위해 키보드 dismiss 범위 수정함. 이를 위해 keyboardDismissableView 생성

--- ONETHING_iOS/ONETHING_iOS/Source/ViewController/HabitWritingViewController.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/ViewController/HabitWritingViewController.swift
@@ -12,16 +12,19 @@ final class HabitWritingViewController: BaseViewController {
     private var completeButton: CompleteButton!
     private let dailyHabitInfoView = DailyHabitView()
     private let viewModel = HabitWritingViewModel()
+    private let keyboardDismissableView = UIView()
     private let habitSelectStampView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     override func viewDidLoad() {
         super.viewDidLoad()
      
         self.addKeyboardDismissTapGesture()
+        self.setupKeyboardDismissableView()
         self.setupBackBtnTitleView()
         self.setupDailyHabitView()
         self.setupCompleteButton()
         self.setupHabitSelectStampView()
+        self.view.bringSubviewToFront(self.dailyHabitInfoView)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,12 +37,21 @@ final class HabitWritingViewController: BaseViewController {
         self.tabBarController?.tabBar.isHidden = false
     }
     
+    private func setupKeyboardDismissableView() {
+        self.keyboardDismissableView.backgroundColor = .red
+        self.view.addSubview(self.keyboardDismissableView)
+        
+        self.keyboardDismissableView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+        }
+    }
+    
     private func setupBackBtnTitleView() {
         self.backBtnTitleView = BackBtnTitleView(parentViewController: self)
         self.backBtnTitleView.update(title: "1일차")
         let safeArea = self.view.safeAreaLayoutGuide
         
-        self.view.addSubview(self.backBtnTitleView)
+        self.keyboardDismissableView.addSubview(self.backBtnTitleView)
         self.backBtnTitleView.snp.makeConstraints {
             $0.top.equalTo(safeArea).offset(54)
             $0.leading.equalToSuperview().offset(32)
@@ -48,10 +60,11 @@ final class HabitWritingViewController: BaseViewController {
     }
     
     private func setupDailyHabitView() {
-        self.view.addSubview(self.dailyHabitInfoView)
+        self.keyboardDismissableView.addSubview(self.dailyHabitInfoView)
         self.dailyHabitInfoView.snp.makeConstraints {
             $0.top.equalTo(self.backBtnTitleView.snp.bottom)
             $0.leading.trailing.equalToSuperview().inset(32)
+            $0.bottom.equalToSuperview().offset(-40)
         }
     }
     
@@ -67,7 +80,7 @@ final class HabitWritingViewController: BaseViewController {
     }
     
     private func setupHabitSelectStampView() {
-        self.habitSelectStampView.backgroundColor = .white
+        self.habitSelectStampView.backgroundColor = .clear
         self.habitSelectStampView.delegate = self
         self.habitSelectStampView.dataSource = viewModel
         self.habitSelectStampView.registerCell(cellType: HabitCalendarCell.self)
@@ -78,6 +91,11 @@ final class HabitWritingViewController: BaseViewController {
             $0.leading.trailing.equalToSuperview().inset(50)
             $0.bottom.equalTo(self.completeButton.snp.top)
         }
+    }
+    
+    override func addKeyboardDismissTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(_:)))
+        self.keyboardDismissableView.addGestureRecognizer(tapGesture)
     }
 }
 
