8fe49155e12239e20be2ea0d7c32129788479b34
ehgud0670 <kimdo2297@gmail.com>
2021-07-20 11:31:55 +0900

    [feature] montserrat 도 폰트 상수로 추가

--- ONETHING_iOS/ONETHING_iOS/Source/Constants/FontName.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/Constants/FontName.swift
@@ -16,6 +16,13 @@ struct FontName {
     static var pretendard_medium: String { return self.prentendard + "-" + Weight.medium.rawValue }
     static var pretendard_regular: String { return self.prentendard + "-" + Weight.regular.rawValue }
     
+    static var montserrat_semibold: String { return self.montserrat + "-" + Weight.semiBold.rawValue }
+    static var montserrat_bold: String { return self.montserrat + "-" + Weight.bold.rawValue }
+    static var montserrat_extrabold: String { return self.montserrat + "-" + Weight.extraBold.rawValue }
+    static var montserrat_light: String { return self.montserrat + "-" + Weight.light.rawValue }
+    static var montserrat_medium: String { return self.montserrat + "-" + Weight.medium.rawValue }
+    static var montserrat_regular: String { return self.montserrat + "-" + Weight.regular.rawValue }
+    
     private static let prentendard = "Pretendard"
     private static let montserrat = "Montserrat"
 
