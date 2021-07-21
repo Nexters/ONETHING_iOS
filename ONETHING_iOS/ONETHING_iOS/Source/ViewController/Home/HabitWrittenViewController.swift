1c371b72ce713154bb8fe52406352493ed7776b3
ehgud0670 <kimdo2297@gmail.com>
2021-07-21 19:18:35 +0900

    [feature] 상단 스탬프 뷰 추가

--- ONETHING_iOS/ONETHING_iOS/Source/ViewController/Home/HabitWrittenViewController.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/ViewController/Home/HabitWrittenViewController.swift
@@ -9,16 +9,30 @@ import UIKit
 
 final class HabitWrittenViewController: BaseViewController {
     private let dailyHabitView = DailyHabitView()
+    private let upperStampView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+        setupUpperStampView()
         setupDailyHabitView()
     }
     
     private func setupView() {
-        self.view.cornerRadius = 50
+        self.view.cornerRadius = 40
+    }
+    
+    private func setupUpperStampView() {
+        self.upperStampView.contentMode = .scaleAspectFit
+        self.upperStampView.image = Stamp.beige.defaultImage
+        
+        self.view.addSubview(self.upperStampView)
+        self.upperStampView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(-28)
+            $0.height.width.equalTo(70)
+            $0.leading.equalToSuperview().inset(32)
+        }
     }
     
     private func setupDailyHabitView() {
