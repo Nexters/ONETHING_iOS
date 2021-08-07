5b1a666836ab73aaafb81a2dcbfb4a38353a0183
dongminyoon <ehdals8965@naver.com>
2021-08-07 17:20:50 +0900

    fix: NetworkInformation 프로퍼티 수정

--- ONETHING_iOS/ONETHING_iOS/Source/Network/NetworkInfomation.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/Network/NetworkInfomation.swift
@@ -7,27 +7,29 @@
 
 import Foundation
 
-enum NetworkInfomation {
-    enum Request {
-        enum HeaderKeys {
-            static let contentType = "Content-type"
-            static let authorization = "Authorization"
-        }
-        
-        enum HeaderValues {
-            static let json = "application/json"
-            static let authorization = "Bearer " + (OnethingUserManager.sharedInstance.accessToken ?? "")
-        }
-        
-        enum ParameterKeys {
-            static let habitId = "habit_id"
-        }
+struct NetworkInfomation {
+    
+    struct HeaderKey {
+        static let contentType = "Content-Type"
+        static let authorization = "Authorization"
     }
+    
+    struct HeaderValue {
+        static let json = "application/json"
+        static var authorization: String { return "Bearer " + (OnethingUserManager.sharedInstance.accessToken ?? "") }
+    }
+    
+    struct ParameterKey {
+        static let habitId = "habit_id"
+    }
+    
 }
 
 extension NetworkInfomation {
+    
     static var headers: [String: String] {
-        return [NetworkInfomation.Request.HeaderKeys.contentType: NetworkInfomation.Request.HeaderValues.json,
-                NetworkInfomation.Request.HeaderKeys.authorization: NetworkInfomation.Request.HeaderValues.authorization]
+        return [NetworkInfomation.HeaderKey.contentType: NetworkInfomation.HeaderValue.json,
+                NetworkInfomation.HeaderKey.authorization: NetworkInfomation.HeaderValue.authorization]
     }
+    
 }
