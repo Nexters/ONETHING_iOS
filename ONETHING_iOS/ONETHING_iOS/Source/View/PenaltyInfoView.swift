2487f1f830547256ea23acdf0aac058ee56a6d2e
ehgud0670 <kimdo2297@gmail.com>
2021-08-25 10:27:41 +0900

    [feature] penaltyInfoView UI 구현

---
+++ ONETHING_iOS/ONETHING_iOS/Source/View/PenaltyInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  PenaltyInfoView.swift
+//  ONETHING_iOS
+//
+//  Created by sdean on 2021/08/25.
+//
+
+import UIKit
+
+final class PenaltyInfoView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+    @IBOutlet weak var countBoxView: UIView!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var arrowImageView: UIImageView!
+    
+}
--- ONETHING_iOS/ONETHING_iOS/Source/ViewController/WritingPenaltyViewController.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/ViewController/WritingPenaltyViewController.swift
@@ -21,7 +21,27 @@ final class WritingPenaltyViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setupPenaltyInfoView()
         self.bindButtons()
+    }
+    
+    private func setupPenaltyInfoView() {
+        guard let penaltyInfoView = self.penaltyInfoView else { return }
+
+        penaltyInfoView.do {
+            $0.isUserInteractionEnabled = false
+            $0.arrowImageView.isHidden = true
+
+        }
+        
+        self.penaltyInfoContainerView.addSubview(penaltyInfoView)
+        penaltyInfoView.snp.remakeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        penaltyInfoView.countBoxView.snp.remakeConstraints {
+            $0.width.equalTo(64)
+        }
     }
     
     private func bindButtons() {
@@ -43,6 +63,9 @@ final class WritingPenaltyViewController: BaseViewController {
     private let disposeBag = DisposeBag()
     
     @IBOutlet private weak var backButton: UIButton!
+    
+    @IBOutlet private weak var penaltyInfoContainerView: UIView!
+    private let penaltyInfoView: PenaltyInfoView? = UIView.createFromNib()
     
     @IBOutlet private weak var completeButton: UIButton!
     @IBOutlet private weak var completeLabel: UILabel!
