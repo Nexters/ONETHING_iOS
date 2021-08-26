0200433febf3fe3e3b3ba15bc670409a80ce8f63
dongminyoon <ehdals8965@naver.com>
2021-08-27 01:39:37 +0900

    [feature] 공지사항 Cell Layout 업데이트 로직 변경

--- ONETHING_iOS/ONETHING_iOS/Source/ViewController/Profile/NoticeViewController.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/ViewController/Profile/NoticeViewController.swift
@@ -25,12 +25,19 @@ final class NoticeViewController: BaseViewController {
         self.tableView.rowHeight          = UITableView.automaticDimension
         self.tableView.estimatedRowHeight = 82
         
-        self.viewModel.noticesRelay.bind(to: self.tableView.rx.items) { tableView, index, item in
+        self.viewModel.noticesRelay.bind(to: self.tableView.rx.items) { [weak self] tableView, index, item in
+            guard let self = self        else { return UITableViewCell() }
+            guard let noticeId = item.id else { return UITableViewCell() }
+            
             let indexPath = IndexPath(row: index, section: 0)
             let cell      = tableView.dequeueReuableCell(cell: NoticeTableViewCell.self, forIndexPath: indexPath)
+            let isExpand  = self.viewModel.expandingSet.contains(noticeId)
             
-            guard let noticeCell = cell else { return UITableViewCell() }
+            guard let noticeCell = cell  else { return UITableViewCell() }
+            noticeCell.delegate = self
             noticeCell.configure(item)
+            noticeCell.updateLayoutForExpand(isExpand, animated: false)
+            
             return noticeCell
         }.disposed(by: self.disposeBag)
     }
@@ -46,5 +53,13 @@ final class NoticeViewController: BaseViewController {
     
     @IBOutlet private weak var backButton: UIButton!
     @IBOutlet private weak var tableView: UITableView!
+    
+}
+
+extension NoticeViewController: NoticeTableViewCellDelegate {
+    
+    func noticeTableViewCell(_ cell: NoticeTableViewCell, isExpanding: Bool, didUpdateExpand notice: NoticeModel) {
+        self.viewModel.updateExpandingStatus(of: notice, isExpanding: isExpanding)
+    }
     
 }
