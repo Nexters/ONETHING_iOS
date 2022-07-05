7f2e69d07e47cd9cb5b7c167677e09e6a0b7f269
ehgud0670 <kimdo2297@gmail.com>
2022-07-05 21:10:51 +0900

    refactor: 함수 분리

--- ONETHING_iOS/ONETHING_iOS/Source/ViewController/MainTabBarController+Launch.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/ViewController/MainTabBarController+Launch.swift
@@ -12,26 +12,33 @@ extension MainTabBarController {
     
     func decideStartController() {
         if OnethingUserManager.sharedInstance.hasAccessToken == false {
-            guard let loginViewController = LoginViewController.instantiateViewController(from: .intro) else { return }
-            guard let navigationController = UIViewController.navigationController(loginViewController) else { return }
-            self.present(navigationController, animated: false)
+            self.presentLoginViewController()
             return
         }
         
         OnethingUserManager.sharedInstance.requestAccount { accountModel in
             guard accountModel.doneHabitSetting == false else { return }
             
-            let goalSettingController = GoalSettingFirstViewController.instantiateViewController(from: .goalSetting)
-            guard let navigationController = UIViewController.navigationController(goalSettingController) else { return }
-            self.present(navigationController, animated: false) {
-                guard accountModel.account?.nickname == nil else { return }
-                
-                let viewController = ProfileSettingViewController.instantiateViewController(from: .intro)
-                guard let profileSettingController = viewController else { return }
-                profileSettingController.modalPresentationStyle = .fullScreen
-                navigationController.present(profileSettingController, animated: false)
-            }
+            self.presentGoalSettingVCOrProfileSettingVC(with: accountModel)
         }
     }
     
+    private func presentLoginViewController() {
+        guard let loginViewController = LoginViewController.instantiateViewController(from: .intro) else { return }
+        guard let navigationController = UIViewController.navigationController(loginViewController) else { return }
+        self.present(navigationController, animated: false)
+    }
+    
+    private func presentGoalSettingVCOrProfileSettingVC(with accountModel: OnethingAccountModel) {
+        let goalSettingController = GoalSettingFirstViewController.instantiateViewController(from: .goalSetting)
+        guard let navigationController = UIViewController.navigationController(goalSettingController) else { return }
+        self.present(navigationController, animated: false) {
+            guard accountModel.account?.nickname == nil else { return }
+            
+            let viewController = ProfileSettingViewController.instantiateViewController(from: .intro)
+            guard let profileSettingController = viewController else { return }
+            profileSettingController.modalPresentationStyle = .fullScreen
+            navigationController.present(profileSettingController, animated: false)
+        }
+    }
 }
