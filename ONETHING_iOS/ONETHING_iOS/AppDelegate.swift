3267aa086942065519606c126b7d6f1e96a60d89
dongminyoon <ehdals8965@naver.com>
2022-09-03 01:38:31 +0900

    feat: Push Alarm AppDelegate 설정 코드 추가

--- ONETHING_iOS/ONETHING_iOS/AppDelegate.swift
+++ ONETHING_iOS/ONETHING_iOS/AppDelegate.swift
@@ -14,6 +14,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        self.setupPushNotification(withApplication: application)
         NetworkDetector.shared.startMonitor()
         SocialManager.sharedInstance.setup()
         return true
@@ -31,10 +32,40 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return [.portrait]
     }
+    
+    private func setupPushNotification(withApplication application: UIApplication) {
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOption: UNAuthorizationOptions = [.alert, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOption) { granted, _ in
+            if granted { print("@Push Authorization was completed") }
+        }
+        application.registerForRemoteNotifications()
+    }
 
 }
 
+extension AppDelegate: MessagingDelegate {
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+        
+        print("@FCM Token : \(fcmToken)")
+    }
+    
+}
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+}
 
 extension UIApplication {
     
