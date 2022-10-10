43beeb618bf13ab8beba325fc6dd3009652a3301
ehgud0670 <kimdo2297@gmail.com>
2022-10-10 16:39:47 +0900

    fix: iOS 16 미만에서도 레이아웃 크래시 안나도록 코드 수정
    * 전에 iOS 16에서의 크래시를 해결하느라 코드 수정을 했는데 이 때문에, 정작 16 미만에서는 크래시가 발생하게 되었습니다.
    * iOS 16과 16 미만 모든 버전에서 모두 크래시 발생하지 않도록 코드 수정합니다!

--- ONETHING_iOS/ONETHING_iOS/Source/View/HabitTextView.swift
+++ ONETHING_iOS/ONETHING_iOS/Source/View/HabitTextView.swift
@@ -16,10 +16,9 @@ final class HabitTextView: UITextView {
     override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
         
-        self.setup()
-        self.setupPlaceholderLabel()
-        self.setupBottomLines()
-        self.setupTextCountLabel()
+        self.setupUI()
+        self.layoutUI()
+        self.layoutManager.delegate = self
     }
     
     required init?(coder: NSCoder) {
@@ -28,62 +27,49 @@ final class HabitTextView: UITextView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        self.addSubview(self.placeholderLabel)
-        self.addSubview(self.firstBottomLine)
-        self.addSubview(self.secondBottomLine)
-        self.addSubview(self.textCountLabel)
-        
-        self.layoutPlaceholderLabel()
-        self.layoutBottomLines()
-        self.layoutTextCountLabel()
+    
+        self.layoutUI()
     }
     
-    private func setup() {
-        self.font = UIFont.createFont(type: .pretendard(weight: .regular), size: 16)
-        self.layoutManager.delegate = self
-        self.delegate = self
-        self.isScrollEnabled = false
-        self.spellCheckingType = .no
-    }
-    
-    private func setupPlaceholderLabel() {
+    private func setupUI() {
+        self.do {
+            $0.font = UIFont.createFont(type: .pretendard(weight: .regular), size: 16)
+            $0.delegate = self
+            $0.isScrollEnabled = false
+            $0.spellCheckingType = .no
+        }
+        
         self.placeholderLabel.do {
             $0.text = "기록하고 싶은 습관 이야기를 적어보세요!"
             $0.font = UIFont.createFont(type: .pretendard(weight: .semiBold), size: 16)
             $0.textColor = UIColor.init(hexString: "D7D7D7")
             $0.isHidden = false
         }
-    }
-    
-    private func setupBottomLines() {
+        
         self.firstBottomLine.backgroundColor = .black_10
         self.secondBottomLine.backgroundColor = .black_10
-    }
-    
-    private func setupTextCountLabel() {
+        
         self.textCountLabel.do {
             $0.textColor = .black_40
             $0.font = UIFont.createFont(type: .montserrat(weight: .medium), size: 10)
             $0.textAlignment = .center
             $0.text = countText
         }
+        
+        self.addSubview(self.placeholderLabel)
+        self.addSubview(self.firstBottomLine)
+        self.addSubview(self.secondBottomLine)
+        self.addSubview(self.textCountLabel)
     }
     
-    private var countText: String {
-        return "\(self.text.count) / \(self.textLimit)"
-    }
-    
-    private func layoutPlaceholderLabel() {
+    private func layoutUI() {
         let beginPosition = self.beginningOfDocument
         let beginOrigin = self.caretRect(for: beginPosition).origin
         self.placeholderLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(beginOrigin.x)
             $0.top.equalToSuperview().offset(beginOrigin.y)
         }
-    }
-    
-    private func layoutBottomLines() {
+        
         self.firstBottomLine.snp.makeConstraints {
             $0.top.equalTo(self.placeholderLabel.snp.bottom).offset(10)
             $0.leading.equalToSuperview()
@@ -97,14 +83,16 @@ final class HabitTextView: UITextView {
             $0.width.equalToSuperview()
             $0.height.equalTo(1)
         }
-    }
-    
-    private func layoutTextCountLabel() {
+        
         self.textCountLabel.snp.makeConstraints {
             $0.trailing.equalTo(self.firstBottomLine)
             $0.top.equalTo(self.firstBottomLine.snp.bottom)
             $0.bottom.equalTo(self.secondBottomLine.snp.top)
         }
+    }
+
+    private var countText: String {
+        return "\(self.text.count) / \(self.textLimit)"
     }
 }
 
